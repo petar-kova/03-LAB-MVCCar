2d60447f9a1fe8bbd8c626df4ea18f10bef097a1
Petar <pekovac@vsite.hr>
2025-11-19 18:32:16 +0100

    popravak labosa- petar kovač

--- MVCCar/Controllers/CarsController.cs
+++ MVCCar/Controllers/CarsController.cs
@@ -54,7 +54,7 @@ namespace MVCCar.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Color,ProductionYear,Price")] Car car)
+        public async Task<IActionResult> Create([Bind("Id,Name,Color,ProductionYear,Price,Manufacturer")] Car car)
         {
             if (ModelState.IsValid)
             {
@@ -86,7 +86,7 @@ namespace MVCCar.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Color,ProductionYear,Price")] Car car)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Color,ProductionYear,Price,Manufacturer")] Car car)
         {
             if (id != car.Id)
             {
--- MVCCar/Models/Car.cs
+++ MVCCar/Models/Car.cs
@@ -3,10 +3,12 @@
     public class Car
     {
         public int Id { get; set; }
+        
         public string Name { get; set; } = string.Empty;
         public string Color { get; set; } = string.Empty;
         public int ProductionYear { get; set; }
         public decimal Price { get; set; }
+
+        public string Manufacturer { get; set; } = string.Empty;
     }
 }
-public string Manufacturer { get; set; } = string.Empty;
